f7c374468294c6da5806d348f3ef272a1dc06d67
Kendall Kehbuma <126034797+yourfavengineer@users.noreply.github.com>
2024-02-02 01:53:07 -0800

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
 provider "aws" {
-  region = "us-west-2"
+  region                   = "eu-central-1"
 }
